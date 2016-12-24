a625a0f5774c607f64d921da1597ba2479aa76f5
Keivan <me@keivan.io>
2016-12-24 12:08:48 -0800

    Added support for reading distro information from /etc/*-release

---
+++ src/NzbDrone.Mono.Test/EnvironmentInfo/LinuxVersionInfoFixture.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Mono.EnvironmentInfo;
+using NzbDrone.Test.Common;
+
+namespace NzbDrone.Mono.Test.EnvironmentInfo
+{
+    public class LinuxVersionInfoFixture : TestBase<LinuxVersionInfo>
+    {
+        [Test]
+        public void should_get_version_info()
+        {
+            Subject.FullName.Should().NotBeNullOrWhiteSpace();
+            Subject.Name.Should().NotBeNullOrWhiteSpace();
+            Subject.Version.Should().NotBeNullOrWhiteSpace();
+        }
+    }
+}
---
+++ src/NzbDrone.Mono/EnvironmentInfo/LinuxVersionInfo.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace NzbDrone.Mono.EnvironmentInfo
+{
+    public class LinuxVersionInfo
+    {
+        public LinuxVersionInfo()
+        {
+            var releaseFiles = Directory.GetFiles("/etc/", "*-release").ToList();
+            releaseFiles.ForEach(ImportValues);
+        }
+
+        private void ImportValues(string releaseFile)
+        {
+            var lines = File.ReadAllLines(releaseFile);
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split('=');
+                if (parts.Length >= 2)
+                {
+                    var key = parts[0];
+                    var value = parts[1];
+
+                    if (!string.IsNullOrWhiteSpace(value))
+
+                    {
+                        switch (key)
+                        {
+                            case "ID":
+                                Name = value;
+                                break;
+                            case "PRETTY_NAME":
+                                FullName = value;
+                                break;
+                            case "VERSION_ID":
+                                Version = value;
+                                break;
+
+                        }
+                    }
+                }
+
+            }
+        }
+
+        public string Version { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string FullName { get; private set; }
+    }
+}
