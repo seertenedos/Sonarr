64c6ad5624516a04a91fb13472b66f496c4967ac
Keivan Beigi <me@keivan.io>
2016-12-23 21:09:52 -0800

    Get proper .net framework version

---
+++ src/NzbDrone.Windows.Test/EnvironmentInfo/DotNetRuntimeProviderFixture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using NzbDrone.Test.Common;
+
+namespace NzbDrone.Windows.EnvironmentInfo
+{
+    public class DotNetRuntimeProviderFixture : TestBase<DotNetRuntimeProvider>
+    {
+        [Test]
+        public void should_get_framework_version()
+        {
+            var version = new Version(Subject.RuntimeVersion);
+            version.Major.Should().Be(4);
+            version.Minor.Should().BeOneOf(0, 5, 6);
+        }
+    }
+}
--- src/NzbDrone.Windows/EnvironmentInfo/DotNetRuntimeProvider.cs
+++ src/NzbDrone.Windows/EnvironmentInfo/DotNetRuntimeProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Win32;
 using NLog;
 using NzbDrone.Common.EnvironmentInfo;
 
@@ -9,8 +10,51 @@ namespace NzbDrone.Windows.EnvironmentInfo
         public DotNetRuntimeProvider(Common.IServiceProvider serviceProvider, Logger logger)
             : base(serviceProvider, logger)
         {
+            RuntimeVersion = GetFrameworkVersion();
         }
 
-        public override string RuntimeVersion => Environment.Version.ToString();
+        public override string RuntimeVersion { get; }
+
+        private static string GetFrameworkVersion()
+        {
+            const string subkey = @"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\";
+            using (var ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey(subkey))
+            {
+                if (ndpKey == null)
+                {
+                    return "4.0";
+                }
+
+                var releaseKey = (int)ndpKey.GetValue("Release");
+
+
+                if (releaseKey >= 394802)
+                {
+                    return "4.6.2";
+                }
+                else if (releaseKey >= 394254)
+                {
+                    return "4.6.1";
+                }
+                else if (releaseKey >= 393295)
+                {
+                    return "4.6";
+                }
+                else if (releaseKey >= 379893)
+                {
+                    return "4.5.2";
+                }
+                else if (releaseKey >= 378675)
+                {
+                    return "4.5.1";
+                }
+                else if (releaseKey >= 378389)
+                {
+                    return "4.5";
+                }
+
+                return "4.0";
+            }
+        }
     }
 }
